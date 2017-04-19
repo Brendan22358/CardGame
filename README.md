c2900d3251e6c1fe59eaedb310009bd0d41556d4
Brendan22358 <bgallagher@sjnma.org>
2017-04-19 10:45:46 -0500

    [I have no idea what I'm Doing]

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CardGame
-Card game using the classes we created in class
+Card game using the classes we created in class. I am making a game of war. Each player draws a card from the top of the deck and the higher number wins.
---
+++ hand.py
@@ -0,0 +1,15 @@
+from card import Card
+
+class Hand:
+	def __init__(self):
+		self.player1hand=[]
+		self.player2hand=[]
+		for i in range (1,27):
+			self.player1hand.append(i)
+		for i in range (27,53):
+			self.player2hand.append(i)
+		for card1 in self.player2hand:
+			player1cards=
+		print(self.player1hand)
+		print(self.player2hand)
+Motivation_Check=Hand()
